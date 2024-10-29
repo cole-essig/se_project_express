854fd361b24785143f785ad7dbe16cd128bd3fcc
Cole Essig <hoopwalker@yahoo.com>
2024-10-28 17:39:56 -0700

    updated index routes for signup signin, added middleware folder for authorization

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 const { notFoundError } = require("../utils/errors")
+const { login, createUser } = require('../controllers/users');
 
 const userRouter = require('./users');
 const itemRouter = require('./clothingitems');
 
 router.use('/users', userRouter);
 router.use('/items', itemRouter);
+router.post('/signin', login);
+router.post('/signup', createUser);
 router.use((req, res) => {
   res
     .status(notFoundError)
