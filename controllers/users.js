063bb7276f73b2ec38b04e585e1e3ae1c2b5e2c4
Cole Essig <hoopwalker@yahoo.com>
2024-11-01 15:22:26 -0700

    Corrections made in users controller, added a catch in login and simplfied error blocks in other contrpollers

--- controllers/users.js
+++ controllers/users.js
@@ -1,34 +1,8 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user')
-const { invalidDataError, notFoundError, serverError, conflictError } = require("../utils/errors");
+const { invalidDataError, notFoundError, serverError, conflictError, unauthorizedError } = require("../utils/errors");
 const JWT_SECRET = require('../utils/config')
-
-const getUsers = (req, res) => {
-  User.find({})
-  .then((users) => res.send(users))
-  .catch((err) => {
-    console.error(err);
-    return res.status(serverError).send({message: "An error has occurred on the server"});
-  })
-}
-
-const getUser = (req, res) => {
-  const { userId } = req.params;
-  User.findById(userId)
-  .orFail()
-  .then((user) => res.send(user))
-  .catch((err) => {
-    console.error(err);
-    if (err.name === 'DocumentNotFoundError') {
-      return res.status(notFoundError).send({message: err.message});
-    }
-    if (err.name === "CastError") {
-      return res.status(invalidDataError).send({message: "Invalid data"});
-    }
-    return res.status(serverError).send({message: "An error has occurred on the server"});
-  })
-};
 
 const createUser = (req, res) => {
   const { name, avatar, email, password } = req.body;
@@ -43,10 +17,6 @@ const createUser = (req, res) => {
         return res.status(invalidDataError).send({ message: "Invalid data" });
       } if (err.code === 11000) {
         return res.status(conflictError).send({ message: "User already exists" });
-      } if (err.code === 409) {
-        return res.status(409).send({message: "This email already exists"})
-      } if (err.name === 'CastError') {
-        return res.status(invalidDataError).send({message: "This email or password doesn't exist"})
       }
       return res.status(serverError).send({ message: "An error has occurred on the server" });
     });
@@ -54,6 +24,11 @@ const createUser = (req, res) => {
 
 const login = (req, res) => {
   const {email, password} = req.body;
+
+  if (!email || !password) {
+    return res.status(BAD_REQUEST).send({message: "the password and email are required"});
+  }
+
   User.findUserByCredentials(email, password)
   .then((user) => {
     const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
@@ -63,12 +38,8 @@ const login = (req, res) => {
   })
   .catch((err) => {
     console.error(err)
-    if (err.name === 'ValidationError') {
-      return res.status(invalidDataError).send({message: "Invalid data"});
-    } if (err.name === '11000') {
-      return res.status(invalidDataError).send({message: "User or Password already exist"});
-    } if (err) {
-      return res.status(invalidDataError).send({message: "Incorrect email or password"});
+    if (err.message === 'Incorrect email or password') {
+      return res.status(unauthorizedError).send({message: "Invalid data"});
     }
     return res.status(serverError).send({message: "An error has occurred on the server"});
   })
@@ -85,9 +56,6 @@ const getCurrentUser = (req, res) => {
     })
     .catch((err) => {
       console.error(err);
-      if (err.name === 'DocumentNotFoundError') {
-        return res.status(notFoundError).send({ message: "User not found" });
-      }
       return res.status(serverError).send({ message: "An error has occurred on the server" });
     });
 };
@@ -105,9 +73,6 @@ const updateProfile = (req, res) => {
     })
     .catch((err) => {
       console.error(err);
-      if (err.name === 'DocumentNotFoundError') {
-        return res.status(notFoundError).send({ message: "User not found" });
-      }
       if (err.name === "ValidationError") {
         return res.status(invalidDataError).send({ message: "Invalid data" });
       }
