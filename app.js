3d3c9e77eb7c59103b64c6ef1885c5b0aef66b65
Cole Essig <hoopwalker@yahoo.com>
2024-12-03 18:58:46 -0700

    added validation middleware, implemented into app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require('cors');
 const mainRouter = require('./routes/index');
 const errorHandler = require('./middlewares/error-handler')
+const { errors } = require('celebrate');
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db')
@@ -15,6 +16,7 @@ const { PORT = 3001 } = process.env;
 app.use(express.json());
 app.use(cors());
 app.use("/", mainRouter);
+app.use(errors());
 app.use(errorHandler);
 
 if (process.env.NODE_ENV !== "test") {
