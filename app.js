dfbfb586470d6a286373f290df6d48f8b5d7d0c3
Cole Essig <hoopwalker@yahoo.com>
2024-12-15 13:22:51 -0700

    Added a logging system for all requests. errors and all. updated app.js to use logger

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const mainRouter = require('./routes/index');
 const errorHandler = require('./middlewares/error-handler')
 const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db')
@@ -15,7 +16,9 @@ const { PORT = 3001 } = process.env;
 
 app.use(express.json());
 app.use(cors());
+app.use(requestLogger);
 app.use("/", mainRouter);
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
